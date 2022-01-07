59405c9c930d3d8932224bae339787475ae13d21
Andrew English <aje299@gmail.com>
2022-01-07 13:55:35 -0800

    Update title and logos

--- static/src/components/navbar/index.js
+++ static/src/components/navbar/index.js
@@ -1,12 +1,14 @@
-import { Flex, Box, Heading } from 'theme-ui';
+import { Flex, Box, Heading, Image } from 'theme-ui';
 import WalletButton from '../WalletButton';
+import SplitbaseLogo from '../../images/splitbase_logo.png';
 
 export default function NavBar() {  
   return (
     <Flex sx={{ alignItems: 'center', width: '100%', mb: '5' }} maxHeight='5rem'>
+      <Image src={SplitbaseLogo} sx={{ height: ['48px', '54px'] }} />
       <Box sx={{ px: 2, flex: 1 }}>
         <Heading as="h1" textAlign='center'>
-          Some Name
+          Splitbase
         </Heading>
       </Box>
       <Flex sx={{ p: 2, alignItems: 'center', columnGap: '10px' }}>
